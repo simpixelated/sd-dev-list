3eb1be1d652ce3e4c114828858299065e618cb10
Jordan Kohl <kohl.jordan@gmail.com>
2020-01-02 19:03:48 -0800

    adds timeline events to display

--- src/App.js
+++ src/App.js
@@ -3,6 +3,37 @@ import groupBy from 'lodash/groupBy';
 import kebabCase from 'lodash/kebabCase';
 import projects from './projects.json';
 
+const ProjectCard = ({ project }) => (
+  <div className="card mb-2">
+    <div className="card-body">
+      <h5 className="card-title">{project.title || `${project.neighborhood} ${project.type}`}</h5>
+      {project.neighborhood && <h6 className="card-subtitle mb-2 text-muted">{project.neighborhood}</h6>}
+      {project.location && <p className="card-subtitle mb-2 text-muted"><small>{project.location}</small></p>}
+      {project.description && <p className="card-text">{project.description}</p>}
+      {project.events &&
+        <dl>
+          {project.events.sort((a, b) => Object.values(a)[0] - Object.values(b)[0]).map(event => (
+            <React.Fragment key={Object.keys(event)[0]}>
+              <dt>{Object.keys(event)[0]}</dt>
+              <dd>{Object.values(event)[0]}</dd>
+            </React.Fragment>
+          ))}
+        </dl >
+      }
+      {project.address && <a className="card-link" href={`https://www.google.com/maps/place/${project.address}`}>Map</a>}
+      {project.links &&
+        <ul>
+          {project.links.map(link => (
+            <li key={link}>
+              <a href={link}>{link}</a>
+            </li>
+          ))}
+        </ul>
+      }
+    </div>
+  </div>
+)
+
 function App() {
   const groupedProjects = groupBy(projects, 'neighborhood')
   return (
@@ -21,27 +52,10 @@ function App() {
       </ul>
       <div className="project-list">
       {Object.keys(groupedProjects).map(neighborhood => (
-        <div>
+        <div key={neighborhood}>
           <h4 className="my-4" id={kebabCase(neighborhood)}>{neighborhood}</h4>
           {groupedProjects[neighborhood].map((project, index) => (
-            <div className="card mb-2" key={index}>
-              <div className="card-body">
-                <h5 className="card-title">{project.title || `${project.neighborhood} ${project.type}`}</h5>
-                {project.neighborhood && <h6 className="card-subtitle mb-2 text-muted">{project.neighborhood}</h6>}
-                {project.location && <p className="card-subtitle mb-2 text-muted"><small>{project.location}</small></p>}
-                {project.description && <p className="card-text">{project.description}</p>}
-                {project.address && <a className="card-link" href={`https://www.google.com/maps/place/${project.address}`}>Map</a>}
-                {project.links &&
-                  <ul>
-                    {project.links.map(link => (
-                      <li key={link}>
-                        <a href={link}>{link}</a>
-                      </li>
-                    ))}
-                  </ul>
-                }
-              </div>
-            </div>
+            <ProjectCard project={project} key={index} />
           ))}
         </div>
       ))}
